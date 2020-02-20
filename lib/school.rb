4023ace1a20518e721ecc4ab819f7e2b63b36273
AndrewDAntonio <github email address>
2020-02-19 22:42:49 -0500

    Complete

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,37 @@
-# code here!
+require 'pry'
+
+class School
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+
+  end
+
+  def roster
+     @roster
+  end
+
+  def add_student(name, grade)
+    if !@roster[grade]
+        @roster[grade] = []
+    end
+    
+    @roster[grade] << name
+
+   end
+
+   def grade (grade)
+    @roster[grade]
+   end
+
+   def sort
+     @roster.each do |key, value|
+       @roster[key].sort!
+
+     end 
+
+   end 
+  
+
+end
